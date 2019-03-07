42740c38e7bcd81570368fa4f0f3574ab4014977
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2019-03-07 22:12:39 +0300

    finished

--- app/src/main/java/com/sadogan/androidhw0023/LoginActivity.kt
+++ app/src/main/java/com/sadogan/androidhw0023/LoginActivity.kt
@@ -1,38 +1,31 @@
 package com.sadogan.androidhw0023
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.annotation.TargetApi
-import android.content.pm.PackageManager
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.app.LoaderManager.LoaderCallbacks
-import android.content.CursorLoader
-import android.content.Loader
-import android.database.Cursor
-import android.net.Uri
-import android.os.AsyncTask
-import android.os.Build
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.text.TextUtils
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.widget.ArrayAdapter
-import android.widget.TextView
 
-import java.util.ArrayList
-import android.Manifest.permission.READ_CONTACTS
 import android.widget.AdapterView
 import android.widget.Spinner
 
 import kotlinx.android.synthetic.main.activity_login.*
+import pl.aprilapps.easyphotopicker.EasyImage
+import pl.aprilapps.easyphotopicker.DefaultCallback
+import android.content.Intent
+import java.io.File
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import java.util.*
+
 
 /**
  * A login screen that offers login via email/password.
  */
 class LoginActivity : AppCompatActivity() {
 
+
+    var selectedCity: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,74 +49,131 @@ class LoginActivity : AppCompatActivity() {
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var v : Any = parent!!.getItemAtPosition(position)
+                selectedCity = v as String
+                name.error = selectedCity
+                name.requestFocus()
 
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
 
         }
 
+        selectedCity = resources.getStringArray(R.array.cities_array)[0]
 
+        photo_button.setOnClickListener {
+            EasyImage.openChooserWithGallery( this, "Choose an image", 0)
+        }
+
+        birthdate.maxDate = Calendar.getInstance().timeInMillis
 
     }
 
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
+
     private fun attemptSave() {
 
         // Reset errors.
-        email.error = null
-        password.error = null
+        name.error = null
+        surname.error = null
+        tcid.error = null
 
-        // Store values at the time of the login attempt.
-        val emailStr = email.text.toString()
-        val passwordStr = password.text.toString()
 
-        var cancel = false
-        var focusView: View? = null
+        val nameStr = name.text.toString()
+        val surnameStr = surname.text.toString()
+        val tcidStr = tcid.text.toString()
+        val birthdateStr = "${birthdate.dayOfMonth} / ${birthdate.month + 1} / ${birthdate.year}"
 
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(passwordStr) && !isPasswordValid(passwordStr)) {
-            password.error = getString(R.string.error_invalid_password)
-            focusView = password
-            cancel = true
+
+        if(TextUtils.isEmpty(nameStr)){
+            name.error = getString(R.string.error_field_required)
+            name.requestFocus()
+            return
         }
 
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(emailStr)) {
-            email.error = getString(R.string.error_field_required)
-            focusView = email
-            cancel = true
-        } else if (!isEmailValid(emailStr)) {
-            email.error = getString(R.string.error_invalid_email)
-            focusView = email
-            cancel = true
+        if (containsDigit(nameStr)) {
+            name.error = getString(R.string.error_name_cannot_contain_digits)
+            name.requestFocus()
+            return
         }
 
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView?.requestFocus()
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            //TODO go to success page
+        if(TextUtils.isEmpty(surnameStr)){
+            surname.error = getString(R.string.error_field_required)
+            surname.requestFocus()
+            return
         }
+
+        if (containsDigit(surnameStr)) {
+            surname.error = getString(R.string.error_name_cannot_contain_digits)
+            surname.requestFocus()
+            return
+        }
+
+        if(TextUtils.isEmpty(tcidStr)){
+            tcid.error = getString(R.string.error_field_required)
+            tcid.requestFocus()
+            return
+        }
+
+        if(!TextUtils.isDigitsOnly(tcidStr)){
+            tcid.error = getString(R.string.error_must_only_contain_digis)
+            tcid.requestFocus()
+            return
+        }
+
+        if(tcidStr.length != 11){
+            tcid.error = getString(R.string.error_must_be_11_in_lengt)
+            tcid.requestFocus()
+            return
+        }
+
+
+        val myIntent = Intent(this, ResultActivity::class.java)
+        myIntent.putExtra("name", nameStr)
+        myIntent.putExtra("surname", surnameStr)
+        myIntent.putExtra("tcid", tcidStr)
+        myIntent.putExtra("birthdate", birthdateStr)
+        myIntent.putExtra("birthplace", selectedCity)
+        myIntent.putExtra("photo_path", photoPath)
+
+        this.startActivity(myIntent)
+
     }
 
-    private fun isEmailValid(email: String): Boolean {
-        //TODO: Replace this with your own logic
-        return email.contains("@")
+    private fun containsDigit(s: String?): Boolean {
+        var containsDigit = false
+
+        if (s != null && !s.isEmpty()) {
+            for (c in s.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    containsDigit = true
+                    break
+                }
+            }
+        }
+
+        return containsDigit
     }
 
-    private fun isPasswordValid(password: String): Boolean {
-        //TODO: Replace this with your own logic
-        return password.length > 4
+    var photoPath : String? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, object : DefaultCallback() {
+            override fun onImagePicked(imageFile: File?, source: EasyImage.ImageSource?, type: Int) {
+                val options = BitmapFactory.Options()
+                options.inPreferredConfig = Bitmap.Config.ARGB_8888
+                photoPath = imageFile?.path
+                val bitmap = BitmapFactory.decodeFile(imageFile?.path, options)
+                photo_view.setImageBitmap(bitmap)
+            }
+
+            override fun onImagePickerError(e: Exception?, source: EasyImage.ImageSource?, type: Int) {
+                //Some error handling
+            }
+
+        })
     }
 
 
---
+++ app/src/main/java/com/sadogan/androidhw0023/ResultActivity.kt
@@ -0,0 +1,41 @@
+package com.sadogan.androidhw0023
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import kotlinx.android.synthetic.main.activity_results.*
+
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_results)
+
+        val nameStr = intent.getStringExtra("name")
+        val surnameStr = intent.getStringExtra("surname")
+        val tcidStr = intent.getStringExtra("tcid")
+        val birthdate = intent.getStringExtra("birthdate")
+        val birthplaceStr = intent.getStringExtra("birthplace")
+        val photoPathStr = intent.getStringExtra("photo_path")
+
+        result_name.text = "Name: $nameStr"
+        result_surname.text = "Surname: $surnameStr"
+        result_tcid.text = "TC ID: $tcidStr"
+        result_birthdate.text = "Birthdate: $birthdate"
+        result_birthplace.text = "Birthplace: $birthplaceStr"
+
+        val options = BitmapFactory.Options()
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888
+        val bitmap = BitmapFactory.decodeFile(photoPathStr, options)
+        result_photo_view.setImageBitmap(bitmap)
+
+        back_button.setOnClickListener {
+            finish()
+        }
+
+    }
+
+
+}
